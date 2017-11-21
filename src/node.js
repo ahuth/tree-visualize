cbc969e987495930c21ce157945c160cbfc3b241
Andrew Huth <andrew@huth.me>
2017-11-21 15:14:06 -0800

    Add insert and iterator functionality to our tree

--- src/node.js
+++ src/node.js
@@ -4,3 +4,31 @@ export default function Node(value, parent = null) {
   this.left = null;
   this.right = null;
 }
+
+Node.prototype.insert = function (value) {
+  if (this.value === value) {
+    return this;
+  }
+  if (value < this.value) {
+    if (this.left) {
+      return this.left.insert(value);
+    }
+    this.left = new Node(value);
+    return this.left;
+  }
+  if (this.right) {
+    return this.right.insert(value);
+  }
+  this.right = new Node(value);
+  return this.right;
+};
+
+Node.prototype[Symbol.iterator] = function* () {
+  if (this.left) {
+    yield* this.left[Symbol.iterator]();
+  }
+  yield this.value;
+  if (this.right) {
+    yield* this.right[Symbol.iterator]();
+  }
+};
--- src/reducers/values.js
+++ src/reducers/values.js
@@ -1,6 +1,11 @@
 import Node from '../node';
 
 const initialState = new Node(5);
+initialState.insert(7);
+initialState.insert(4);
+initialState.insert(2);
+initialState.insert(6);
+initialState.insert(3);
 
 export default function values(state = initialState, action) {
   switch (action.type) {
