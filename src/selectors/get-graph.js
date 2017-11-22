d7f4be08ed7b3c239cf8fb14713d75dabfd003de
Andrew Huth <andrew@huth.me>
2017-11-22 09:36:12 -0800

    Add selector for computing a "graph" of the nodes and edges

---
+++ src/selectors/get-graph.js
@@ -0,0 +1,37 @@
+export default function getGraph(state) {
+  const tree = state.values;
+  return {
+    nodes: getNodes(tree),
+    edges: getEdges(tree),
+  };
+}
+
+function getNodes(tree) {
+  const nodes = [];
+
+  for (let node of tree) {
+    nodes.push({
+      id: node.value,
+      label: node.value,
+    });
+  }
+
+  return nodes;
+}
+
+function getEdges(tree) {
+  const edges = [];
+
+  for (let node of tree) {
+    const parent = node.parent
+    if (parent) {
+      edges.push({
+        id: `${parent.value}_to_${node.value}`,
+        source: parent.value,
+        target: node.value,
+      });
+    }
+  }
+
+  return edges;
+}
--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,1 +1,2 @@
 export { default as getValues } from './get-values';
+export { default as getGraph } from './get-graph';
