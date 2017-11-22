6d370d8656c8b6d721507f52a86708f8b3a19414
Andrew Huth <andrew@huth.me>
2017-11-21 16:29:03 -0800

    Render a table with information about each node

--- src/components/app.js
+++ src/components/app.js
@@ -3,8 +3,27 @@ import map from 'lodash.map';
 
 export default function App({ values }) {
   return (
-    <ul>
-      {map(values, node => <li key={node.value}>{node.value}</li>)}
-    </ul>
+    <table>
+      <thead>
+        <tr>
+          <th>Value</th>
+          <th>Parent</th>
+          <th>Left</th>
+          <th>Right</th>
+          <th>Root</th>
+        </tr>
+      </thead>
+      <tbody>
+        {map(values, node => (
+          <tr key={node.value}>
+            <td>{node.value}</td>
+            <td>{node.parent}</td>
+            <td>{node.left}</td>
+            <td>{node.right}</td>
+            <td>{node.parent ? undefined : 'Yes'}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
