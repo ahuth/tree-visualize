499dd51553cf162b0a86b4aa9fcd1fba0ce75e99
Andrew Huth <andrew@huth.me>
2017-11-25 10:44:27 -0800

    Add basic <App> container

---
+++ src/containers/app.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import App from '../components/app';
+
+function mapStateToProps(state) {
+  return {};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
