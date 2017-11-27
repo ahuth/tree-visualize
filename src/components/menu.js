e2255c1a68d1b36d1590a693205dae458e5f1d15
Andrew Huth <andrew@huth.me>
2017-11-26 16:58:02 -0800

    Add a basic-styled menu without functionality

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
+const styles = {
+  container: {
+    backgroundColor: 'white',
+    border: '1px solid black',
+    listStyle: 'none',
+    margin: 0,
+    padding: 0,
+  },
+  item: {
+    cursor: 'pointer',
+    padding: '0.4rem',
+  },
+};
+
 export default function Menu() {
   return (
-    <div>I'm a menu!</div>
+    <ul style={styles.container}>
+      <li style={styles.item}>Rotate left</li>
+      <li style={styles.item}>Rotate right</li>
+    </ul>
   );
 }
